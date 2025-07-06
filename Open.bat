9f06363da67a51472ff7c31842d5e6a3a8b3efec
KaloyanSyS32 <kaloyankrastev2013@gmail.com>
2025-07-06 14:48:00 +0300

    Updated Open.bat

--- Open.bat
+++ Open.bat
@@ -1,3 +1,17 @@
-pip install pygame
-timeout 5
-python doombench.py
+@echo off
+title Doom2DBench
+cls
+echo ===========================
+echo         doom2dbench
+echo ===========================
+echo.
+timeout 3 >nul
+cls
+echo Checking for pygame package 
+pip install pygame >nul
+timeout 3 >nul
+cls
+timeout 3 >nul
+cls
+python doombench.py >nul
+cls
